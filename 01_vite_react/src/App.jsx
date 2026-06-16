403150217176ad4a2699c1399133bd1f2e8d3354
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-06-16 11:03:51 +0530

    basics2

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -116,7 +116,10 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+function App1() {
+  return <code></code>;
+}
+export default App1;
---
+++ 01_vite_react/src/code1.js
@@ -0,0 +1,7 @@
+function code() {
+  return <h2>Code is ready and by catherine</h2>;
+}
+
+export default code;
+
+// SPA - single page application
